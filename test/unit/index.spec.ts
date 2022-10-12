24dedab95a42bfd1d343873830374602d9a8892d
Josh Bowling <josh@monarci.com>
2022-10-12 12:17:35 +0900

    Update channel name, slug, and query
    Revert channel slug back to channel-user-feedback so as to not
    cause user interruption for those who frequent the channel. Update the
    query to match both old user-feedback contracts and the new
    typeform-feedback contracts. Will restrict to typeform-feedback
    contracts once the type transition is complete.
    
    Change-type: major
    Signed-off-by: Josh Bowling <josh@monarci.com>

--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -6,7 +6,7 @@ const pluginManager = new PluginManager([feedbackPlugin()]);
 test('Expected contracts are loaded', () => {
 	const contracts = pluginManager.getCards();
 	expect(Object.keys(contracts).includes('typeform-feedback')).toBe(true);
-	expect(Object.keys(contracts).includes('channel-feedback')).toBe(true);
+	expect(Object.keys(contracts).includes('channel-user-feedback')).toBe(true);
 });
 
 test('Expected integrations are loaded', () => {
