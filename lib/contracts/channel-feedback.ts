1f695e0dcb2cc7a0b75deccc9a4c7779d858374e
Josh Bowling <josh@monarci.com>
2022-10-13 14:51:28 +0900

    Add channel
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@monarci.com>

---
+++ lib/contracts/channel-feedback.ts
@@ -0,0 +1,24 @@
+import type { ContractDefinition } from 'autumndb';
+
+export const channelFeedback: ContractDefinition = {
+	slug: 'channel-feedback',
+	name: 'Feedback',
+	type: 'channel@1.0.0',
+	markers: ['org-balena'],
+	data: {
+		filter: {
+			name: 'Feedback contracts',
+			schema: {
+				type: 'object',
+				additionalProperties: true,
+				required: ['type'],
+				properties: {
+					type: {
+						type: 'string',
+						enum: ['user-feedback@1.0.0', 'typeform-feedback@1.0.0'],
+					},
+				},
+			},
+		},
+	},
+};
