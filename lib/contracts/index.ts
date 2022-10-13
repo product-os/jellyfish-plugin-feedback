4b6c06e088a3a64c5923f2b02ee4a2d45d6499fd
Josh Bowling <josh@monarci.com>
2022-10-13 14:07:26 +0900

    Add relationships
    Change-type: minor
    Signed-off-by: Josh Bowling <josh@monarci.com>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -1,4 +1,10 @@
 import type { ContractDefinition } from 'autumndb';
+import { relationshipPatternIsAttachedToTypeformFeedback } from './relationship-pattern-is-attached-to-typeform-feedback';
+import { relationshipTypeformFeedbackIsOwnedByUser } from './relationship-typeform-feedback-is-owned-by-user';
 import { typeformFeedback } from './typeform-feedback';
 
-export const contracts: ContractDefinition[] = [typeformFeedback];
+export const contracts: ContractDefinition[] = [
+	relationshipPatternIsAttachedToTypeformFeedback,
+	relationshipTypeformFeedbackIsOwnedByUser,
+	typeformFeedback,
+];
